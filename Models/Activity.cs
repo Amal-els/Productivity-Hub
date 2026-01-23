4581bb3d4dd0e9918030f29c51a1b14527b7e6d9
dorra dorra <dorra.ourabi@insat.ucar.tn>
2026-01-23 22:45:47 +0100

    added the class Activity in working on the TO-DO-LIST

---
+++ Models/Activity.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamProject.Models;
+
+public class Activity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime dateOfCreation{ get; set; }
+    public DateTime? dateOfCompletion { get; set; }
+    public String? Acheived { get; set; }
+}
