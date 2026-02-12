238fecd423452a31a650e6ff92fa55088c272662
Amal-els <amalbahri19@gmail.com>
2026-02-12 14:43:04 +0100

    last merge

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -13,17 +13,13 @@ namespace TeamProject.Models;
         public string? LastName { get; set; }
 
         public string FullName => $"{FirstName} {LastName}".Trim();
-<<<<<<< ours
 
-        public string? EmailPassword { get; set; }
 
         public string? ContactsSheetId{ get; set; }
         // Navigation properties
-=======
         public Guid? toDoListId { get; set; }
         public virtual toDoList? doList { get; set; }
         public virtual ICollection<Note> Notes { get; set; }
->>>>>>> theirs
         public virtual ICollection<CalendarEvent> CalendarEvents { get; set; }
         public virtual ICollection<EventParticipant> EventParticipations { get; set; }
         public virtual ICollection<Meeting> OrganizedMeetings { get; set; }
